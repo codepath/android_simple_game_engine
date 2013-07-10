79ccb56361d510561510f285a2346f264c262541
Nathan Esquenazi <nesquena@gmail.com>
2013-07-10 16:52:31 -0700

    Separate positioned actor and moving actors

--- src/com/codepath/simplegame/actors/SimpleMovingActor.java
+++ src/com/codepath/simplegame/actors/SimpleMovingActor.java
@@ -1,19 +1,12 @@
 package com.codepath.simplegame.actors;
 
-import android.graphics.Point;
-import android.graphics.Rect;
-import android.graphics.RectF;
-
 import com.codepath.simplegame.Velocity;
 
-public abstract class SimpleMovingActor extends Actor {
-	private Point pos;
-	private int height;
-	private int width;
+public abstract class SimpleMovingActor extends PositionedActor {
 	private Velocity velocity;
 
 	public SimpleMovingActor(int x, int y) {
-		this.pos = new Point(x, y);
+		super(x, y);
 		this.velocity = new Velocity();
 	}
 	
@@ -25,63 +18,12 @@ public abstract class SimpleMovingActor extends Actor {
 	// Called to move position based on the velocity
 	public void move() {
 		if (this.isEnabled()) {
-			pos.x += (velocity.getXSpeed() * velocity.getXDirection());
-			pos.y += (velocity.getYSpeed() * velocity.getYDirection());
+			getPoint().x += (velocity.getXSpeed() * velocity.getXDirection());
+			getPoint().y += (velocity.getYSpeed() * velocity.getYDirection());
 		}
-	}
-	
-	public int getWidth() {
-		return this.width;
-	}
-	
-	public int getHeight() {
-		return this.height;
-	}
-	
-	public void setDimensions(int width, int height) {
-		this.width = width;
-		this.height = height;
 	}
 	
 	public Velocity getVelocity() {
 		return velocity;
-	}
-	
-	public Point getPoint() {
-		return pos;
-	}
-	
-	public Rect getRect() {
-		Rect rect = new Rect();
-		getRectF().round(rect);
-		return rect;
-	}
-	
-	public RectF getRectF() {
-		return new RectF(getX(), getY(), getX() + width, getY() + height);
-	}
-	
-	public void setPos(int x, int y) {
-		this.pos = new Point(x, y);
-	}
-
-	public int getX() {
-		return this.pos.x;
-	}
-
-	public void setX(int x) {
-		this.pos.x = x;
-	}
-
-	public int getY() {
-		return this.pos.y;
-	}
-
-	public void setY(int y) {
-		this.pos.y = y;
-	}
-	
-	public boolean intersect(SimpleMovingActor actor) {
-		return getRect().intersect(actor.getRect());
 	}
 }
